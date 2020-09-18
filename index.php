fab9bc07fd39b47d3eb9db579b300e7211e0b5fa
Cristyan Tibaquicha <cdtibaquicha19@gmail.com>
2020-09-17 20:48:40 -0500

    Clase de 17/09/2020 - Funciones base de php

--- index.php
+++ index.php
@@ -4,11 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Demo curso laravel</title>
-
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous"> 
-        
-
-
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -50,11 +46,12 @@
                   $num2 = 1 ;
                  // $resultado = $num1 + $num2  ; 
 
+                 $num1++;
                  echo " El resultado  de la suma  es : ". ($num1+$num2) ."<br>"; 
                  echo " El resultado  de la resta  es : ". ($num1-$num2) ."<br>"; 
                  echo " El resultado  de la multiplicacion  es : ". ($num1*$num2) ."<br>"; 
-                 echo " El resultado  de la exponenciacion  es : ". ($num1**$num2) ."<br>"; 
-                 
+                 echo " El resultado  de la division  es : ". ($num1/$num2) ."<br>"; 
+                                 
 
                 ?>
               </div>
@@ -63,12 +60,7 @@
 
 
 
-          </div>
-
-
-        
-
-
+          </div>    
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
